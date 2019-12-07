09e707498bffdce39e4a1fe7271601391559d2cb
Marius Muntean <marius.muntean@live.com>
2019-12-07 08:50:03 +0100

    Project setup for LSM9DS1

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.IMU.LSM9DS1/Driver/Sensors.IMU.LSM9DS1/Sensors.IMU.LSM9DS1.cs
@@ -0,0 +1,10 @@
+﻿namespace Sensors.IMU.LSM9DS1
+{
+    public class LSM9DS1
+    {
+
+        public LSM9DS1()
+        {
+        }
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/Sensors.IMU.LSM9DS1/Samples/Sensors.IMU.KSM9DS1_Sample/Sensors.IMU.KSM9DS1_Sample.cs
@@ -0,0 +1,16 @@
+﻿using Meadow;
+using Meadow.Devices;
+
+namespace Sensors.IMU.KSM9DS1_Sample
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+
+
+        public MeadowApp()
+        {
+          
+        }
+
+    }
+}
